9882b76afe39f6aec4bdc607b5dc1bf9caaeb87a
Marcos Antonio Butler Torres III <rumblereflex@gmail.com>
2025-10-25 18:15:07 +0300

    Initial Commit

---
+++ SwiftCamp/SwiftCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCampApp.swift
+//  SwiftCamp
+//
+//  Created by Heru on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftCampTests/SwiftCampTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCampTests.swift
+//  SwiftCampTests
+//
+//  Created by Heru on 10/25/25.
+//
+
+import Testing
+@testable import SwiftCamp
+
+struct SwiftCampTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
